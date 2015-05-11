4c24c90f53e18b1d8b1b0f88dddde3651196cc6a
liamtreacy <liam.treacy@gmail.com>
2015-05-11 22:01:39 +0100

    Basic functional client
    Script now creates the desired database with an auto increment id which
    I was having problems with earlier… Script now takes user options,
    allowing the user to add to the db or view its contents.

--- trackOutgoings.py
+++ trackOutgoings.py
@@ -19,18 +19,37 @@ def connectToAndValidateDb():
         
     except:
         print "Can't find table in database, creating a new one..."
-        cur.execute("CREATE TABLE Expenses(Id INT, Amount REAL, Description TEXT, Date TEXT)")
+        cur.execute("CREATE TABLE Expenses(ID INTEGER PRIMARY KEY AUTOINCREMENT, Amount REAL, Description TEXT, Date TEXT)")
+
 
 def addExpense():
-    #amount = raw_input("Add expense (for today), amount: ")
-    #description = raw_input("Description: ")
-    #date = time.strftime("%x")
+    amount      = raw_input("Expense amount: ")
+    description = raw_input("Description   : ")
     
-    #row = [2, date ,description ,amount ]
+    date = time.strftime("%x")
+    
+    newRow = [amount, description, date]
     con = sqlite3.connect('outgoings.db')
     cur = con.cursor()
-    cur.execute("INSERT INTO Expenses VALUES(1, 25.97 ,'misc', 'today')")
+    cur.execute("INSERT INTO Expenses (Amount, Description, Date) VALUES (? ,?, ?)",newRow)
     con.commit()
+
+
+def viewDb():
+    print 'Viewing db...'
+    con = sqlite3.connect('outgoings.db')
+    cur = con.cursor()
+    rows = cur.execute("SELECT * FROM Expenses")
+
+    for idx, val in enumerate(rows):
+        print "\n\t---------START ROW-----------------"
+        print "\t\tid:\t",val[0]
+        print "\t\tAmount:\t",val[1]
+        print "\t\tDesc:\t",val[2]
+        print "\t\tDate:\t",val[3]
+        print "\n\t----------END ROW------------------"
+
+    print "Db viewing over..."
 
 
 def presentUserOptions():
@@ -44,17 +63,30 @@ def presentUserOptions():
         if input == "1":
             addExpense()
         elif input == "2":
-            print "2"
+            viewDb()
         elif input == "0":
             print "Exiting..."
             if con:
                 con.close()
 
 
+def testDatabase():
+    print 'testing'
+    con = sqlite3.connect('outgoings.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO Expenses VALUES(1, 25.97 ,'misc', 'today')")
+    print 'Inserted'
+
+    cur.execute("INSERT INTO Expenses (Amount, Description, Date) VALUES (25.97 ,'desc_test', 'date_text')")
+    rows = cur.execute("SELECT * FROM Expenses")
+    
+    for i in rows:
+        print i
+
+
 def main():
     connectToAndValidateDb()
     presentUserOptions()
-
 
 if __name__ == "__main__":
     main()
