539fbc930d718024156c91eb835018cfdacb6b41
Liam Treacy <liam.treacy@gmail.com>
2021-10-29 08:52:02 +0100

    Moved CsvBuilder into its own file

--- test_csv_builder.py
+++ test_csv_builder.py
@@ -1,45 +1,7 @@
 import unittest
 
-from enum import Enum
+from csv_builder import CsvBuilder, CsvEntry, FlowType, MoneyArea, PayeeName
 
-class PayeeName(Enum):
-    LIAM = "Liam"
-    CHRISTY = "Christy"
-    OTHER = "OTHER"
-
-class FlowType(Enum):
-    INCOME = "INCOME"
-    OUTGOING = "OUTGOING"
-
-class MoneyArea(Enum):
-    WORK = "WORK"
-    OTHER = "OTHER"
-    HOUSE = "HOUSE"
-    CAR = "CAR"
-    SAVINGS = "SAVINGS"
-    GROCERIES = "GROCERIES"
-
-class CsvEntry:
-    def __init__(self, flow_type, money_area, amount, payee, desc=""):
-        self.flow_type = flow_type
-        self.money_area = money_area
-        self.amount = amount
-        self.desc = desc
-        self.payee = payee
-
-    def to_csv_row_str(self):
-        return f"{self.flow_type.value},\"{self.money_area.value}\",{self.amount:.2f},\"{self.payee.value}\",\"{self.desc}\""
-
-class CsvBuilder:
-    def __init__(self):
-        self.header_row = ["FlowType,SourceType,Amount,Payee,Desc"]
-        self.rows = []
-
-    def add_entry(self, csv_entry):
-        self.rows.append(csv_entry.to_csv_row_str())
-
-    def build(self):
-        return '\n'.join(self.header_row + self.rows)
 
 class CsvBuilderTestCase(unittest.TestCase):
     def test_creation(self):
