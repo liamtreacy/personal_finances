210713fdd485fbc615acd2a5f9cdd55c6a0c534c
Liam Treacy <liam.treacy@gmail.com>
2021-11-01 19:37:09 +0000

    More functionality and tests around PayeeEnumName

--- test_payee_name.py
+++ test_payee_name.py
@@ -3,36 +3,44 @@ import unittest
 from enum import Enum
 
 class PayeeNameEnum(Enum):
-    __order__ = 'LIAM CHRISTY OTHER'
-    LIAM = 0
-    CHRISTY = 1
-    OTHER = 2
+    __order__ = 'OTHER LIAM CHRISTY'
+    OTHER = 0
+    LIAM = 1
+    CHRISTY = 2
 
-def PayeeNameEnumToString(payee_name_enum):
-    if payee_name_enum == PayeeNameEnum.LIAM:
-        return "Liam"
-    if payee_name_enum == PayeeNameEnum.CHRISTY:
-        return "Christy"
-    if payee_name_enum == PayeeNameEnum.OTHER:
-        return "Other"
+def number_to_payee(num):
+    return PayeeNameEnum(num)
 
-def StringtoPayeeNameEnum(payee_str):
-    if payee_str.lower() == "liam":
-        return PayeeNameEnum.LIAM
-    if payee_str.lower() == "christy":
-        return PayeeNameEnum.CHRISTY
-    if payee_str.lower() == "other":
-        return PayeeNameEnum.OTHER
+def payee_to_string(payee_name_enum):
+    return payee_name_enum.name
 
-def get_payee_name_from_user():
+def string_to_payee(payee_str):
+    return PayeeNameEnum[payee_str.upper()]
+
+def print_name_and_value():
+    str = ""
     for val in PayeeNameEnum:
-        print(f"{val.key} , {val.value}")
+        str +=f"{val.value} for {val.name} : "
+    print(str)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        get_payee_name_from_user()
-        self.assertEqual(True, False)  # add assertion here
+class PayeeEnumAndFunctionsTestCase(unittest.TestCase):
+    def test_PayeeNameEnum_creation_from_number_other(self):
+        self.assertEqual(PayeeNameEnum.OTHER, number_to_payee(0))
 
+    def test_PayeeNameEnum_creation_from_number_christy(self):
+        self.assertEqual(PayeeNameEnum.CHRISTY, number_to_payee(2))
+
+    def test_PayeeNameEnum_creation_from_string_other(self):
+        self.assertEqual(PayeeNameEnum(0), string_to_payee('OTHER'))
+
+    def test_PayeeNameEnum_creation_from_string_christy_lower(self):
+        self.assertEqual(PayeeNameEnum(2), string_to_payee('christy'))
+
+    def test_PayeeNameEnum_creation_from_string_christy_mixed(self):
+        self.assertEqual(PayeeNameEnum(2), string_to_payee('ChriSty'))
+
+    def test_Payee_to_string(self):
+        self.assertEqual("LIAM", payee_to_string(PayeeNameEnum(1)))
 
 if __name__ == '__main__':
     unittest.main()
